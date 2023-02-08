7c41601e1f3821ed42f73f449ff4bc2942bf8f06
augusto-moraes <moraestf.augusto@gmail.com>
2023-02-08 15:53:21 +0100

    .gitignore update

--- Cours/crazy-sine/crazy-sine.ino
+++ Cours/crazy-sine/crazy-sine.ino
@@ -9,8 +9,9 @@ AudioConnection patchCord1(myDsp,0,out,1);
 
 int midiKeys[] = { 72, 12, 20, 24, 67, 12, 20, 24, 64, 12, 20, 24, 69, 12, 20, 12, 71, 12, 20, 12, 70, 12, 69, 12, 20, 12, 67, 16, 76, 16, 79, 16, 81, 12, 20, 12, 77, 12, 79, 12, 20, 12, 76, 12, 20, 12, 72, 12, 74, 12, 71, 12, 20, 24 };
 //int midiKeys[] = {72,67,64,69,71,70,69,67,76,79,81,77,79,76,72,74,71};
+//int timing[] = {3,3,3,3,3,1,3,1,1,1,3,1,3,3,3,3};
 int midiKeysLength;
-int it;
+int count;
 
 float keyToFreq(int key) 
 {
@@ -20,16 +21,16 @@ float keyToFreq(int key)
 void setup() {
   AudioMemory(2);
   audioShield.enable();
-  audioShield.volume(0.5);
+  audioShield.volume(0.3);
 
   midiKeysLength = sizeof(midiKeys)/sizeof(int);
-  it = 0;
+  count = 0;
 }
 
 void loop() {
-  float freqToPlay = keyToFreq(midiKeys[it%midiKeysLength]);
+  float freqToPlay = keyToFreq(midiKeys[count]);
   Serial.println(freqToPlay);
   myDsp.setFreq(freqToPlay);
-  it++;
+  count = (count+1)%midiKeysLength;
   delay(100);
 }
