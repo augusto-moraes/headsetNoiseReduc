050a053c8b8842187ea41112eb740adb5a4fa523
augusto-moraes <moraestf.augusto@gmail.com>
2023-02-08 15:31:27 +0100

    add cours 2 ++

--- Cours/crazy-sine/crazy-sine.ino
+++ Cours/crazy-sine/crazy-sine.ino
@@ -8,10 +8,11 @@ AudioConnection patchCord0(myDsp,0,out,0);
 AudioConnection patchCord1(myDsp,0,out,1);
 
 int midiKeys[] = { 72, 12, 20, 24, 67, 12, 20, 24, 64, 12, 20, 24, 69, 12, 20, 12, 71, 12, 20, 12, 70, 12, 69, 12, 20, 12, 67, 16, 76, 16, 79, 16, 81, 12, 20, 12, 77, 12, 79, 12, 20, 12, 76, 12, 20, 12, 72, 12, 74, 12, 71, 12, 20, 24 };
+//int midiKeys[] = {72,67,64,69,71,70,69,67,76,79,81,77,79,76,72,74,71};
 int midiKeysLength;
 int it;
 
-double keyToFreq(int key) 
+float keyToFreq(int key) 
 {
   return pow(2.0,(key-69.0)/12.0)*440.0;
 }
@@ -19,14 +20,14 @@ double keyToFreq(int key)
 void setup() {
   AudioMemory(2);
   audioShield.enable();
-  audioShield.volume(0.3);
+  audioShield.volume(0.5);
 
   midiKeysLength = sizeof(midiKeys)/sizeof(int);
   it = 0;
 }
 
 void loop() {
-  double freqToPlay = keyToFreq(midiKeys[it%midiKeysLength]);
+  float freqToPlay = keyToFreq(midiKeys[it%midiKeysLength]);
   Serial.println(freqToPlay);
   myDsp.setFreq(freqToPlay);
   it++;
